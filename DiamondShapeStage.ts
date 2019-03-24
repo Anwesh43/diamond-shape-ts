cb764e4e15f431fa1946b1daff4c72139b799dcc
Anwesh43 <anweshthecool0@gmail.com>
2019-03-24 18:11:44 +0530

    adding DSNode class

--- DiamondShapeStage.ts
+++ DiamondShapeStage.ts
@@ -127,3 +127,48 @@ class Animator {
         }
     }
 }
+
+class DSNode {
+
+    prev : DSNode
+    next : DSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new DSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawDSNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DSNode {
+        var curr : DSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
