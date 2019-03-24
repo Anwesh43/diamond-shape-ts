191665558f73d06ebf7c4a1162c5a15046a00a7b
Anwesh43 <anweshthecool0@gmail.com>
2019-03-24 17:21:16 +0530

    adding scale methods

--- DiamondShapeStage.ts
+++ DiamondShapeStage.ts
@@ -9,6 +9,27 @@ const sizeFactor : number = 2.9
 const foreColor : string = "#1976D2"
 const backColor : string = "#BDBDBD"
 
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) : number => {
+    return Math.floor(scale / scDiv)
+}
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class DiamondShapeStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
