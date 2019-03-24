760c004e6aa89b72dd94e23216275f83a95a4399
Anwesh43 <anweshthecool0@gmail.com>
2019-03-24 18:16:24 +0530

    using renderer in Stage

--- DiamondShapeStage.ts
+++ DiamondShapeStage.ts
@@ -59,6 +59,7 @@ const drawDSNode : Function = (context : CanvasRenderingContext2D, i : number, s
 class DiamondShapeStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -70,11 +71,14 @@ class DiamondShapeStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
